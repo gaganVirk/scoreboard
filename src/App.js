087385a3da64150fe116f926f996e7e6eaf01c1e
gagan <gagan@DESKTOP-IMI2772.localdomain>
2021-02-19 12:40:34 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+const players = [
+  {
+    name : "Guil",
+    score : 50,
+    id : 1
+  },
+  {
+    name : "Treasure",
+    score : 85,
+    id : 2
+  },
+  {
+    name : "Ashley",
+    score : 95,
+    id : 3
+  },
+  {
+    name : "James",
+    score : 80,
+    id : 4
+  }
+];
 
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
+    </header>
+  );
+}
+
+const Player = (props) => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        { props.name }
+      </span>
+    
+      <Counter score={ props.score }/>
     </div>
   );
 }
 
-export default App;
+const Counter = (props) => {
+  return (
+     <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ props.score }</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+  );
+}
+
+const App = (props) => {
+  return (
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={ props.initialPlayers.length }/>
+      {/* Players list */}
+      { props.initialPlayers.map( player => 
+        <Player 
+          name={ player.name } 
+          score={ player.score }
+          id={ player.id.toString() } />
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App initialPlayers={players}/>,
+  document.getElementById('root')
+);
+
+export default app;
